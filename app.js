52fbce41d8910dc646bb457a3ef3029998d3b8ab
bousquetb63 <jbousque@asu.edu>
2018-05-19 13:49:17 -0700

    fixed bugs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const settings = require('./settings.json');
 const bot = new Discord.Client({disableEveryone: true});
 bot.on('ready', async () => {
   console.log(`${bot.user.username} is Reporting for duty!`);
-  bot.user.setActivity(`Use @colors to see all the colors you can change too!`)
+  bot.user.setActivity(`Use !colors to see all the colors you can change too!`)
   try {
       let link = await bot.generateInvite(["ADMINISTRATOR"]);
       console.log(link);
@@ -11,18 +11,16 @@ bot.on('ready', async () => {
       console.log(e.stack);
   }
 });
-
 bot.on('message', message => {
-    if (message.channel.type === 'dm') return;
+    if (message.channel.type === 'dm' || !message.channel.name.includes('color-bot')) return;
     if (message.content == "!colors") {
-        message.delete();
-        message.reply("Available colors: red, blue, green, yellow, purple, gold, brown, orange, black, white and rose. Change colors with !<color>.")
+        message.reply("Available colors: red, blue, green, yellow, purple, gold, brown, orange, black, white and rose. Change colors with !<color>, ex. !red, !blue or !orange")
         .then(msg => {
-            msg.delete(5000);
+            msg.delete(30000);
         });
+        message.delete(30000);
         return;
     }
-    console.log(message.channel);
     if (message.content.charAt(0) == '!'){
         let cmd = message.content.split('!');
         var blue = message.guild.roles.find("name", "Blue");
@@ -36,13 +34,13 @@ bot.on('message', message => {
         var black = message.guild.roles.find("name", "Black");
         var white = message.guild.roles.find("name", "White");
         var rose = message.guild.roles.find("name", "Rose");
-        if(cmd[1] == "blue" || cmd[1] == "red" || cmd[1] == "green" || cmd[1] == "yellow" || cmd[1] == "purple" || cmd[1] == "gold" || cmd[1] == "brown" || cmd[1] == "orange" || cmd[1] == "indigo" || cmd[1] == "black" || cmd[1] == "white" || cmd[1] == "rose") {
+        if(cmd[1] == "blue" || cmd[1] == "red" || cmd[1] == "green" || cmd[1] == "yellow" || cmd[1] == "purple" || cmd[1] == "gold" || cmd[1] == "brown" || cmd[1] == "orange" || cmd[1] == "black" || cmd[1] == "white" || cmd[1] == "rose") {
             if(message.member.roles.has(message.guild.roles.find("name", "1").id)){
                 message.reply("Contact an admin if you want to change colors!")
                 .then(msg => {
-                    msg.delete(5000);
+                    msg.delete(30000);
                 });
-                message.delete();
+                message.delete(30000);
                 return;
             }
             if(message.member.roles.has(blue.id)) {
@@ -74,62 +72,64 @@ bot.on('message', message => {
         switch(cmd[1].toLowerCase()) {
             case 'blue':
                 message.guild.member(message.author).addRole(blue);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'red':
                 message.guild.member(message.author).addRole(red);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'green':
                 message.guild.member(message.author).addRole(green);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'yellow':
                 message.guild.member(message.author).addRole(yellow);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'purple':
                 message.guild.member(message.author).addRole(purple);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'gold':
                 message.guild.member(message.author).addRole(gold);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'brown':
                 message.guild.member(message.author).addRole(brown);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'orange':
                 message.guild.member(message.author).addRole(orange);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'black':
                 message.guild.member(message.author).addRole(black);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'white':
                 message.guild.member(message.author).addRole(white);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
             case 'rose':
                 message.guild.member(message.author).addRole(rose);
-                message.delete();
                 message.guild.member(message.author).addRole(message.guild.roles.find("name", "1"));
+                message.delete(30000);
                 return;
         }
     }
-    return;
+});
+bot.on('error', error => {
+  return;
 });
 // login bot with token
 bot.login(settings.secret.botToken);
